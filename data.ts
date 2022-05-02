f76a435e7c7f94c20fd55a196300254ec871a078
AT <atxiesz@gmail.com>
2022-05-02 11:59:37 +0800

    chore: data

--- data.ts
+++ data.ts
@@ -1,10 +1,37 @@
-// 默认选择环境和小程序版本即可
+// 默认命令
 const defaults = {
-  env: 'uat',
-  version: 'dev',
   channel: 'weapp',
+  env: 'dev',
+  version: 'preview',
 };
 
-console.log('%c AT-[ defaults ]-2', 'font-size:13px; background:#de4307; color:#f6d04d;', defaults);
+// preview
+const command_combine = [
+  'npm run build:weapp:dev:preview',
+  'npm run build:weapp:qa:preview',
+  'npm run build:weapp:pre:preview',
+  'npm run build:weapp:prod:preview',
+  'npm run build:alipay:dev:preview',
+  'npm run build:alipay:qa:preview',
+  'npm run build:alipay:pre:preview',
+  'npm run build:alipay:prod:preview',
+  'npm run build:weapp:dev:upload',
+  'npm run build:weapp:qa:upload',
+  'npm run build:weapp:pre:upload',
+  'npm run build:weapp:prod:upload',
+  'npm run build:alipay:dev:upload',
+  'npm run build:alipay:qa:upload',
+  'npm run build:alipay:pre:upload',
+  'npm run build:alipay:prod:upload',
+];
 
-// 后端知道版本后，
+const { env, channel, version } = defaults;
+
+const aa = command_combine.filter((item) => item.includes(env));
+console.log('%c AT-[ aa ]-31', 'font-size:13px; background:#de4307; color:#f6d04d;', aa);
+
+const bb = aa.filter((item) => item.includes(channel));
+console.log('%c AT-[ bb ]-34', 'font-size:13px; background:#de4307; color:#f6d04d;', bb);
+
+const cc = bb.filter((item) => item.includes(version));
+console.log('%c AT-[ cc ]-37', 'font-size:13px; background:#de4307; color:#f6d04d;', cc);
