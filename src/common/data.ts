92f89e2c03ac7e2c85fdb08886604bde0ba2c12d
AT <atxiesz@gmail.com>
2022-05-21 10:38:34 +0800

    feat: 操作修改

--- src/common/data.ts
+++ src/common/data.ts
@@ -1,6 +1,20 @@
+// 环境列表
 export const ENV_LIST = [
   { key: 'uat', value: 'uat' },
   { key: 'qa', value: 'qa' },
   { key: 'pre', value: 'pre' },
   { key: 'prod', value: 'prod' },
+];
+
+// alpha 分支列表
+export const ALPHA_BRANCH = [
+  { key: 'alpha-v0524', value: 'alpha-v0524' },
+  { key: 'alpha-v0607', value: 'alpha-v0607' },
+  { key: 'alpha-v0621', value: 'alpha-v0621' },
+];
+
+// 功能分支列表
+export const FEATURES_BRANCH = [
+  { key: 'feat-0519-修复商品加料-AT', value: 'feat-0519-修复商品加料-AT' },
+  { key: 'feat-0520-加价购-AT', value: 'feat-0520-加价购-AT' },
 ];
--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { PageContainer } from '@ant-design/pro-layout';
 import { Tabs, Form, Select, Button, Radio, Card } from 'antd';
-import { ENV_LIST } from '@/common/data';
+import { ALPHA_BRANCH, ENV_LIST } from '@/common/data';
 // import styles from './Welcome.less';
 
 const { TabPane } = Tabs;
@@ -31,58 +31,12 @@ const Welcome: React.FC = () => {
               autoComplete="off"
             >
               <Form.Item
-                label="环境"
-                name="env"
-                rules={[{ required: true, message: '请选择环境' }]}
-              >
-                <Select>
-                  {ENV_LIST.map((item) => {
-                    return (
-                      <Option key={item.key} value={item.key}>
-                        {item.value}
-                      </Option>
-                    );
-                  })}
-                </Select>
-              </Form.Item>
-              <Form.Item name="channel" label="小程序" wrapperCol={{ span: 16 }}>
-                <Radio.Group>
-                  <Radio value={'weapp'}>微信</Radio>
-                  <Radio value={'alipay'}>支付宝</Radio>
-                </Radio.Group>
-              </Form.Item>
-
-              <Form.Item name="version" label="版本" wrapperCol={{ span: 16 }}>
-                <Radio.Group>
-                  <Radio value={'dev'}>开发版</Radio>
-                  <Radio value={'experience'}>体验版</Radio>
-                </Radio.Group>
-              </Form.Item>
-
-              <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-                <Button type="primary" htmlType="submit">
-                  确认
-                </Button>
-              </Form.Item>
-            </Form>
-          </TabPane>
-          <TabPane tab="指定分支" key="2">
-            <Form
-              name="basic"
-              labelCol={{ span: 4 }}
-              wrapperCol={{ span: 10 }}
-              initialValues={{ version: 'dev', channel: 'weapp' }}
-              onFinish={onFinish}
-              onFinishFailed={onFinishFailed}
-              autoComplete="off"
-            >
-              <Form.Item
                 label="分支"
                 name="branch"
                 rules={[{ required: true, message: '请选择环境' }]}
               >
                 <Select>
-                  {ENV_LIST.map((item) => {
+                  {ALPHA_BRANCH.map((item) => {
                     return (
                       <Option key={item.key} value={item.key}>
                         {item.value}
@@ -90,32 +44,54 @@ const Welcome: React.FC = () => {
                     );
                   })}
                 </Select>
+                {/* <Radio.Group>
+                  <Radio value={'dev'}>开发版</Radio>
+                  <Radio value={'experience'}>体验版</Radio>
+                </Radio.Group> */}
+              </Form.Item>
+              <Form.Item
+                label="是否合并"
+                name="branch"
+                rules={[{ required: true, message: '请选择环境' }]}
+              >
+                <Select>
+                  {ALPHA_BRANCH.map((item) => {
+                    return (
+                      <Option key={item.key} value={item.key}>
+                        {item.value}
+                      </Option>
+                    );
+                  })}
+                </Select>
+
               </Form.Item>
               <Form.Item
                 label="环境"
                 name="env"
                 rules={[{ required: true, message: '请选择环境' }]}
               >
-                <Select>
+                <Select defaultValue={'uat'}>
                   {ENV_LIST.map((item) => {
                     return (
-                      <Option key={item.key} value={item.key}>
+                      <Option disabled={item.key !== 'uat'} key={item.key} value={item.key}>
                         {item.value}
                       </Option>
                     );
                   })}
                 </Select>
               </Form.Item>
-              <Form.Item name="channel" label="小程序" wrapperCol={{ span: 16 }}>
-                <Radio.Group>
-                  <Radio value={'weapp'}>微信</Radio>
-                  <Radio value={'alipay'}>支付宝</Radio>
-                </Radio.Group>
-              </Form.Item>
+
               <Form.Item name="version" label="版本" wrapperCol={{ span: 16 }}>
                 <Radio.Group>
                   <Radio value={'dev'}>开发版</Radio>
                   <Radio value={'experience'}>体验版</Radio>
+                </Radio.Group>
+              </Form.Item>
+
+              <Form.Item name="channel" label="平台" wrapperCol={{ span: 16 }}>
+                <Radio.Group>
+                  <Radio value={'weapp'}>微信</Radio>
+                  <Radio value={'alipay'}>支付宝</Radio>
                 </Radio.Group>
               </Form.Item>
 
