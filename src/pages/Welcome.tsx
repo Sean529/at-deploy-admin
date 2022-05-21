6cbc2300f7e69d590aaed7aa0dd628f637fcd56c
XiaoZhaoxiaaa <xzxfirst@163.com>
2022-05-21 11:50:20 +0800

    feat: 分支合并

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { PageContainer } from '@ant-design/pro-layout';
-import { Tabs, Form, Select, Button, Radio, Card } from 'antd';
-import { ALPHA_BRANCH, ENV_LIST } from '@/common/data';
+import { Tabs, Form, Select, Button, Radio, Card, Switch, Row, Col } from 'antd';
+import { ALPHA_BRANCH, ENV_LIST, FEATURES_BRANCH } from '@/common/data';
 // import styles from './Welcome.less';
 
 const { TabPane } = Tabs;
@@ -25,52 +25,50 @@ const Welcome: React.FC = () => {
               name="basic"
               labelCol={{ span: 4 }}
               wrapperCol={{ span: 10 }}
-              initialValues={{ version: 'dev', channel: 'weapp' }}
+              initialValues={{ version: 'dev', channel: 'weapp', isMerge: '0', env: 'uat' }}
               onFinish={onFinish}
               onFinishFailed={onFinishFailed}
               autoComplete="off"
             >
-              <Form.Item
-                label="分支"
-                name="branch"
-                rules={[{ required: true, message: '请选择环境' }]}
-              >
-                <Select>
-                  {ALPHA_BRANCH.map((item) => {
-                    return (
-                      <Option key={item.key} value={item.key}>
-                        {item.value}
-                      </Option>
-                    );
-                  })}
-                </Select>
-                {/* <Radio.Group>
-                  <Radio value={'dev'}>开发版</Radio>
-                  <Radio value={'experience'}>体验版</Radio>
-                </Radio.Group> */}
-              </Form.Item>
-              <Form.Item
-                label="是否合并"
-                name="branch"
-                rules={[{ required: true, message: '请选择环境' }]}
-              >
-                <Select>
-                  {ALPHA_BRANCH.map((item) => {
-                    return (
-                      <Option key={item.key} value={item.key}>
-                        {item.value}
-                      </Option>
-                    );
-                  })}
-                </Select>
-
+              <Form.Item label="分支">
+                <Row gutter={20}>
+                  <Col span={16}>
+                    <Form.Item name="branch" rules={[{ required: true, message: '请选择分支' }]}>
+                      <Select>
+                        {ALPHA_BRANCH.map((item) => {
+                          return (
+                            <Option key={item.key} value={item.key}>
+                              {item.value}
+                            </Option>
+                          );
+                        })}
+                      </Select>
+                    </Form.Item>
+                  </Col>
+                  <Col span={8}>
+                    <Form.Item name="isMerge">
+                      <Switch checkedChildren="合并" unCheckedChildren="不合并" />
+                    </Form.Item>
+                  </Col>
+                </Row>
+                <Form.Item name="mergeBranch">
+                  <Select>
+                    {FEATURES_BRANCH.map((item) => {
+                      return (
+                        <Option key={item.key} value={item.key}>
+                          {item.value}
+                        </Option>
+                      );
+                    })}
+                  </Select>
+                </Form.Item>
               </Form.Item>
               <Form.Item
                 label="环境"
                 name="env"
                 rules={[{ required: true, message: '请选择环境' }]}
               >
-                <Select defaultValue={'uat'}>
+                <Select>
                   {ENV_LIST.map((item) => {
                     return (
                       <Option disabled={item.key !== 'uat'} key={item.key} value={item.key}>
